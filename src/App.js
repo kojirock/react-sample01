8672f1a12d18fae2517ccca02aa95e482dbcc13a
kojirock <kojirock5260@gmail.com>
2018-05-30 04:22:36 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import TodoInput from './TodoInput';
+import TodoList from './TodoList';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        {
+          title: 'デフォルトTODO',
+          id: 0
+        },
+      ],
+      uniqueId: 1
+    };
+
+    this.addTodo = this.addTodo.bind(this);
+  }
+
+  addTodo(title) {
+    const {
+      tasks,
+      uniqueId,
+    } = this.state;
+
+    tasks.push({
+      title,
+      id: uniqueId
+    });
+
+    this.setState({
+      tasks,
+      uniqueId: uniqueId + 1
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>TODO App</h1>
+        <TodoInput addTodo={this.addTodo} />
+        <TodoList tasks={this.state.tasks} />
+      </div>
+    );
+  }
+}
+export default App;
+
